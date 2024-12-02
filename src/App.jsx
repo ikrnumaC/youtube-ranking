e67c80c483bde01e011aa8d2e58440c9b07ba065
ikrnumaC <beamsellar@gmail.com>
2024-12-02 21:21:52 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import YouTubeDashboard from './components/YouTubeDashboard'
+
+function App() {
+  return (
+    <div>
+      <YouTubeDashboard />
+    </div>
+  )
+}
+
+export default App
